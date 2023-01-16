88b70d7fe043b6a45315c068a7fc8e91e6abbafb
Saullo Reis <saulloreis01@hotmail.com>
2023-01-16 15:35:20 -0300

    aba projetos concluída

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,80 +1,94 @@
+import { useState } from 'react'
 import './styles/styles.sass'
-import pokemon from "../../assets/img/poke-gif.gif"
-import tmdb from "../../assets/img/tmdb-3.gif"
-import galleria from "../../assets/img/galleriagif.gif"
-import lojarpg from "../../assets/img/lojarpggif1.gif"
-import githubsearch from "../../assets/img/github api.gif"
-import codecafe from "../../assets/img/code-cafe.gif"
-import pizzagif from "../../assets/img/pizza-gif.gif"
-import calculadoragif from "../../assets/img/calculadoragif.gif"
 
 
-const morePages = () => {
-    
-}
 
 export const Projects = () => {
+    const [pages, setPages] = useState(1)
+    const Pages = (element) => {
+        if (pages === 1 && element === 'add') {
+            setPages(pages + 1)
+        }
+        if (pages === 2 && element === 'remove') {
+            setPages(pages - 1)
+        }
+
+    }
+    const changeDisplay = () => {
+        if (pages !== 1) {
+            return 'flex'
+        }
+        return 'none'
+    }
     return (
-        <section>
+        <section className='projects'>
             <h1>Projetos</h1>
             <ul>
-                <div>
-                    <a href="https://lovely-starburst-b74520.netlify.app/" target='_blank'>
-                        <li>
-                            <img src={pokemon}></img>
-                            <h3>Projeto Pokedéx</h3>
-                        </li>
-                    </a>
-                    <a href="https://tmdb-project-3-1vwfc82ax-saullo-reis.vercel.app" target='_blank'>
-                        <li>                       
-                            <img src={tmdb}></img>
-                            <h3>Projeto TMDB</h3>
-                        </li>
-                    </a>
-                    <a href="https://saullo-reis.github.io/projeto-inicial-fetch-github-api-main/" target='_blank'>
-                        <li>                           
-                            <img src={githubsearch}></img>
-                            <h3>Projeto Github Search</h3>
-                        </li>
-                    </a>
-                    <a href="https://saullo-reis.github.io/calculadora/" target='_blank'>
-                        <li>                           
-                            <img src={calculadoragif}></img>
-                            <h3>Projeto Calculadora</h3>
-                        </li>
-                    </a>
+                <div className='container'>
+                    <li>
+                        <a href="https://lovely-starburst-b74520.netlify.app/" target='_blank'>
+                            <div className='image poke-img'></div>
+                        </a>
+                        <h3>Projeto Pokedéx</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://tmdb-project-3-1vwfc82ax-saullo-reis.vercel.app" target='_blank'><div className='image tmdb-img'></div></a>
+                        <h3>Projeto TMDB</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://saullo-reis.github.io/projeto-inicial-fetch-github-api-main/" target='_blank'>
+                            <div className='image github-img'></div></a>
+                        <h3>Projeto Github Search</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://saullo-reis.github.io/calculadora/" target='_blank'>
+                            <div className='image calculadora-img'></div></a>
+                        <h3>Projeto Calculadora</h3>
+                    </li>
+
                 </div>
 
-                <div>
-                    <a href="https://saullo-reis.github.io/sass-project/" target='_blank'>
-                        <li>       
-                            <img src={galleria}></img>
-                            <h3>Projeto Galleria (Sass)</h3>
-                        </li>
-                    </a>
-                    <a href="https://saullo-reis.github.io/landing-page-code-cafe/" target='_blank'>
-                        <li>                           
-                            <img src={codecafe}></img>
-                            <h3>Projeto Landing-Page CODE Café</h3>
-                        </li>
-                    </a>
-                    <a href="https://loja-pxtd7lhbg-saullo-reis.vercel.app/" target='_blank'>
-                        <li>                    
-                            <img src={lojarpg}></img>
-                            <h3>Projeto Loja-rpg</h3>
-                        </li>
-                    </a>
-                    <a href="https://github.com/saullo-reis/pizzaria" target='_blank'>
-                        <li>                           
-                            <img src={pizzagif}></img>
-                            <h3>Projeto Pizzaria</h3>
-                        </li>
-                    </a>
+                <div className='container' style={{ display: changeDisplay() }}>
+
+                    <li>
+                        <a href="https://saullo-reis.github.io/sass-project/" target='_blank'>
+                            <div className='image galleria-img'></div></a>
+                        <h3>Projeto Galleria (Sass)</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://saullo-reis.github.io/landing-page-code-cafe/" target='_blank'>
+                            <div className='image codecafe-img'></div></a>
+                        <h3>Projeto Landing-Page CODE Café</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://loja-pxtd7lhbg-saullo-reis.vercel.app/" target='_blank'>
+                            <div className='image lojarpg-img'></div></a>
+                        <h3>Projeto Loja-rpg</h3>
+                    </li>
+
+                    <li>
+                        <a href="https://github.com/saullo-reis/pizzaria" target='_blank'>
+                            <div className='image pizzaria-img'></div></a>
+                        <h3>Projeto Pizzaria</h3>
+                    </li>
+
                 </div>
             </ul>
-            <button onClick={() => {
-                morePages();
-            }}>Mostrar Mais</button>
+            <div>
+                <button style={{ opacity: pages === 1 && '0.5' }} className='button-projects' onClick={() => {
+                    Pages('remove');
+                }}>Mostrar Menos</button>
+
+                <button style={{ opacity: pages === 2 && '0.5' }} className='button-projects' onClick={() => {
+                    Pages('add');
+                }}>Mostrar Mais</button>
+            </div>
+
         </section>
     )
 }
