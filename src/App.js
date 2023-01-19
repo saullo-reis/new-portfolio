5e81cbb19d313d46fc1823ebdea2e5e154dc2008
Saullo Reis <saulloreis01@hotmail.com>
2023-01-19 17:23:40 -0300

    footer realizado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { About } from "./components/about/about";
 import { Projects } from "./components/projects/projects";
 import { Skills } from "./components/skills/skill";
 import { Email } from "./components/email/email";
+import { Footer } from "./components/footer/footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Projects />
       <Skills />
       <Email/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,9 @@
+import "./styles.sass/styles.sass"
+
+export const Footer = () => {
+    return(
+        <section className="footer">
+            <p className="footer-text"><span>Saullo Reis</span> &copy; 2023</p>
+        </section>
+    )
+}
